88134be21307e329f9d980efb6b87631e55ec69c
Gust Who? <lucgustga@outlook.com>
2025-01-29 15:44:31 +0800

    mod icons

--- src/mods.py
+++ src/mods.py
@@ -1,4 +1,6 @@
+import os
 import utils
+from PIL import Image
 
 mods = {
     "autoplay": "AT",
@@ -28,6 +30,23 @@ for i in range(10):
     mods[f"key{i}"] = f"{i}K"
 
 def create_mods():
+    icons = os.listdir("assets/mods/icons")
     for name in mods.keys():
         short = mods[name]
-        utils.save_hd_sd(utils.text_to_image(short,"assets/ComfortaaBold.ttf",100,"#FFFFFF",True),f"build/selection-mod-{name}.png")
+        background = Image.open("assets/mods/background.png")
+        text = utils.text_to_image(short,"assets/ComfortaaBold.ttf",60,"#FFFFFF",True)
+        text.thumbnail((170,85))
+        background.paste(text,(0,85),text)
+        
+        icon = False
+        if short.lower() + ".png" in icons:
+            icon = Image.open(f"assets/mods/icons/{short.lower()}.png")
+        elif short.lower().endswith("k"):
+            icon = Image.open(f"assets/mods/icons/key.png")
+        
+        if icon:
+            icon.thumbnail((170,45))
+            background.paste(icon,(0,20),icon)
+
+        
+        utils.save_hd_sd(background,f"build/selection-mod-{name}.png")
