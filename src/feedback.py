91a749ebc505079e0db62217010f55f7ea1fbf00
Gust Who? <lucgustga@outlook.com>
2025-02-03 16:56:18 +0800

    blah

--- src/feedback.py
+++ src/feedback.py
@@ -1,8 +1,7 @@
 import utils
 from PIL import Image
 
-def animate_for(frames, path, new):
-    image = Image.open(path)
+def animate_for(frames, image, new):
     utils.remove(utils.add_before(new,f"-{frames}"))
     for i in range(frames):
         utils.save_hd_sd(image,utils.add_before(new,f"-{i}"))
@@ -15,12 +14,12 @@ def all_modes(image,std_name):
     utils.save_hd_sd(image,taiko_name)
 
 def create_feedback():
-    animate_for(15,"assets/gameplay/standard/feedback-miss.png","build/hit0.png")
+    animate_for(15,Image.open("assets/gameplay/standard/feedback-miss.png"),"build/hit0.png")
 
-    animate_for(10,"assets/gameplay/standard/feedback-50.png","build/hit50.png")
+    animate_for(10,Image.open("assets/gameplay/standard/feedback-50.png"),"build/hit50.png")
     
-    animate_for(10,"assets/gameplay/standard/feedback-100.png","build/hit100.png")
-    animate_for(10,"assets/gameplay/standard/feedback-100.png","build/hit100k.png")
+    animate_for(10,Image.open("assets/gameplay/standard/feedback-100.png"),"build/hit100.png")
+    animate_for(10,Image.open("assets/gameplay/standard/feedback-100.png"),"build/hit100k.png")
     
     utils.remove("build/hit300-0.png")
     utils.remove("build/hit300k-0.png")
