79b2623eb06e9ed83b62d5772ec7f7dc955fc20d
Gust Who? <lucgustga@outlook.com>
2025-02-08 21:51:02 +0800

    theme change+mania overhaul

--- src/mania.py
+++ src/mania.py
@@ -15,12 +15,18 @@ def note(main_color):
     
     return outer
 
+colors = {
+    "1": "#ACC9FF",
+    "2": "#7B6CF0",
+    "S": "#FFABF7"
+}
+
 def create_mania():
-    inner = note("#ACC9FF")
-    outer = note("#FFE1AB")
-    special = note("#FFABF7")
+    inner = note(colors["2"])
+    outer = note(colors["1"])
+    special = note(colors["S"])
     rec = Image.open("assets/gameplay/mania/rec.png")
-    rec_held = Image.open("assets/gameplay/mania/rec_held.png")
+    rec_held_overlay = Image.open("assets/gameplay/mania/rec_held.png")
     
     hold = Image.open("assets/gameplay/mania/hold.png")
     hold_end = Image.open("assets/gameplay/mania/hold_end.png")
@@ -39,7 +45,11 @@ def create_mania():
     
     for i in "12S":
         utils.save_hd_sd(rec,f"build/mania-key{i}.png")
-        utils.save_hd_sd(rec_held,f"build/mania-key{i}D.png")
+        this_rec_held_overlay = rec_held_overlay.copy()
+        this_rec = rec.copy()
+        utils.tint_image(this_rec_held_overlay,Color(colors[i]).convert("srgb").coords())
+        this_rec.paste(this_rec_held_overlay,(0,0),this_rec_held_overlay)
+        utils.save_hd_sd(this_rec,f"build/mania-key{i}D.png")
         
         utils.save_hd_sd(hold,f"build/mania-note{i}L.png")
         utils.save_hd_sd(hold_end,f"build/mania-note{i}T.png")
